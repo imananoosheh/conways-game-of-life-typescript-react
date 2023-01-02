b6baaf7920a1c50e88d59c47d9fedd8d927779a2
Iman Anooshehpour <imananoosheh@gmail.com>
2023-01-01 21:42:53 -0800

    added the latest and unfinished changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import produce from "immer";
 
 function App() {
     const numberOfRows = 50;
     const numberOfColumns = 50;
-    const [clickedCell, setClickedCell] = useState([])
+    const [clickedCell, setClickedCell] = useState([]);
     const [grid, setGrid] = useState(() => {
         // initializing 50X50 cells filled with 0
         const rows = [];
@@ -15,6 +15,10 @@ function App() {
         return rows;
     });
 
+    // custom patterns are:
+    // ['blinker', 'toad', 'beacon', pulsar, 'penta-decathlon', 'glider']
+    const [customPattern, setCustomPattern] = useState('')
+    const [clickListen, setClickListen] = useState(false);
     const [running, setRunning] = useState(false);
 
     const runningRef = useRef();
@@ -71,6 +75,28 @@ function App() {
         setTimeout(runSimulation, 100);
     }, []);
 
+    useEffect(()=>{
+        if (clickedCell.length !== 0) {
+            let gridCopy = [...grid];
+            const blinkerOperations = [
+                [0, 0, 1],
+                [0, 1, 1],
+                [0, -1, 1],
+                [-1, 0, 0],
+                [1, 0, 0],
+                [-1, -1, 0],
+                [-1, 1, 0],
+                [1, 1, 0],
+                [1, -1, 0],
+            ];
+            const [clickedX, clickedY] = clickedCell;
+            blinkerOperations.forEach(([x, y, value]) => {
+                gridCopy[clickedX + x][clickedY + y] = value;
+            });
+            setGrid(gridCopy);
+        }
+    },[clickedCell])
+
     return (
         <>
             <button
@@ -91,14 +117,15 @@ function App() {
                         // initializing 50X50 cells filled with 0
                         const rows = [];
                         for (let i = 0; i < numberOfRows; i++) {
-                          let col = []
-                          for(let j=0; j<numberOfColumns;j++){
-                            col[j] = Math.random() > 0.7 ? 1 : 0
-                          }
-                          rows.push(col)
+                            let col = [];
+                            for (let j = 0; j < numberOfColumns; j++) {
+                                col[j] = Math.random() > 0.7 ? 1 : 0;
+                            }
+                            rows.push(col);
                         }
                         return rows;
                     });
+                    setClickListen(false)
                 }}
             >
                 random
@@ -114,33 +141,39 @@ function App() {
                         }
                         return rows;
                     });
-                    setRunning(false)
+                    setRunning(false);
                 }}
             >
                 clear
             </button>
 
-            <button onClick={()=>{
-              if(clickedCell !== []){
-                let gridCopy = [...grid]
-                const blinkerOperations = [
-                  [0,0,1],
-                  [0,1,1],
-                  [0,-1,1],
-                  [-1,0,0],
-                  [1,0,0],
-                  [-1,-1,0],
-                  [-1,1,0],
-                  [1,1,0],
-                  [1,-1,0],
-                ]
-                const [clickedX, clickedY] = clickedCell
-                blinkerOperations.forEach(([x,y,value]) =>{
-                  gridCopy[clickedX+x][clickedY+y] = value
-                })
-                setGrid(gridCopy)
-              }
-            }}>add blinker</button>
+            <button
+                onClick={() => {
+                    setClickListen(!clickListen);
+                    setCustomPattern('blinker')
+                    // if (clickedCell !== []) {
+                    //     let gridCopy = [...grid];
+                    //     const blinkerOperations = [
+                    //         [0, 0, 1],
+                    //         [0, 1, 1],
+                    //         [0, -1, 1],
+                    //         [-1, 0, 0],
+                    //         [1, 0, 0],
+                    //         [-1, -1, 0],
+                    //         [-1, 1, 0],
+                    //         [1, 1, 0],
+                    //         [1, -1, 0],
+                    //     ];
+                    //     const [clickedX, clickedY] = clickedCell;
+                    //     blinkerOperations.forEach(([x, y, value]) => {
+                    //         gridCopy[clickedX + x][clickedY + y] = value;
+                    //     });
+                    //     setGrid(gridCopy);
+                    // }
+                }}
+            >
+                add blinker
+            </button>
 
             <div className="grid-container">
                 {grid.map((rows, r) =>
@@ -148,10 +181,18 @@ function App() {
                         <div
                             className="grid-cell"
                             key={`${r}-${c}`}
-                            onClick={() => {
-                                let gridCopy = [...grid];
-                                gridCopy[r][c] = gridCopy[r][c] === 0 ? 1 : 0;
-                                setClickedCell([r,c])
+                            data={`${r}-${c}`}
+                            onClick={(e) => {
+                                const keys = e.target.getAttribute("data").split('-').map(num => Number(num))
+                                // console.log(keys)
+                                const gridCopy = [...grid];
+                                let [coordX,coordY] = keys
+                                console.log(gridCopy[coordX][coordY])
+                                gridCopy[coordX][coordY] = gridCopy[coordX][coordY] === 0 ? 1 : 0;
+                                console.log(gridCopy[coordX][coordY])
+                                if (clickListen) {
+                                    setClickedCell([coordX, coordY]);
+                                }
                                 setGrid(gridCopy);
                             }}
                             style={{
